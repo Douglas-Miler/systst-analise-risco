0cf7047d3887176406baa5e491a221c9e36fe6aa
Douglas Miler <douglas.junior@bertini.com.br>
2020-10-23 05:53:17 -0300

    Inclusão do Swagger

--- src/main/java/br/com/tst/config/CorsConfiguration.java
+++ src/main/java/br/com/tst/config/CorsConfiguration.java
@@ -2,11 +2,9 @@ package br.com.tst.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-@EnableWebMvc
 public class CorsConfiguration implements WebMvcConfigurer {
 
     @Override
--- src/main/java/br/com/tst/config/SwaggerConfiguration.java
+++ src/main/java/br/com/tst/config/SwaggerConfiguration.java
@@ -2,9 +2,7 @@ package br.com.tst.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Import;
 
-import springfox.bean.validators.configuration.BeanValidatorPluginsConfiguration;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.spi.DocumentationType;
@@ -13,16 +11,18 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @Configuration
 @EnableSwagger2
-@Import(BeanValidatorPluginsConfiguration.class)
 public class SwaggerConfiguration {
 
     @Bean
     public Docket apiV1() {
         return new Docket(DocumentationType.SWAGGER_2)
-                .groupName("v1")
                 .select()
                 .apis(RequestHandlerSelectors.basePackage("br.com.tst"))
                 .build()
-                .apiInfo(new ApiInfoBuilder().title("Title").description("Description").build());
+                .apiInfo(new ApiInfoBuilder()
+        			.title("Sistema de Análise de Risco API")
+        			.description("API responsável por cadastrar e pesquisar veículos, baseando-se na classificação do risco.")
+        			.version("1.0.0").build()
+                );
     }
 }
